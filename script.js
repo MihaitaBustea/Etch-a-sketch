b70c4589d902a184a0de5344c9dea45b4ca8a2ab
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-07-29 18:51:00 +0300

    Add the button to change the size of the grid, style the page and add the functions to generate a new grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var nrOfRows = prompt("How many rows and columns should the grid contain ?");
+var nrOfRows = document.getElementById("grid-size").defaultValue;
 
 function createGrid(x){
+
     var dimension = 640/x;
     for(var rows=0; rows < x; rows++){
         for(var col=0; col < x; col++){
@@ -14,3 +15,23 @@ function createGrid(x){
 }
 
 createGrid(nrOfRows);
+
+function clearGrid(){
+    var oldGrid = document.querySelectorAll(".grid");
+    oldGrid.forEach(element => {
+        element.remove();
+    });
+}
+
+function newGrid(){
+    var newNrOfRows = document.getElementById("grid-size").value; 
+    if(newNrOfRows>100){
+        alert("No dimensions above 100x100 is allowed!");
+    } else {
+        clearGrid(); 
+        createGrid(newNrOfRows);
+    }
+}
+
+var newGridButton = document.getElementById("submit");
+newGridButton.addEventListener("click", newGrid);
